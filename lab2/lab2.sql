4bd937cbb8daeff309d1f216a71cb2dd4b3ec374
Fred Nordell <dat15fno@student.lu.se>
2020-02-07 13:03:31 +0100

    started with script

---
+++ lab2/lab2.sql
@@ -0,0 +1,38 @@
+CREATE TABLE THEATRE (
+    name TEXT,
+    capacity INTEGER,
+    PRIMARY KEY (name)
+);
+
+CREATE TABLE SCREENINGS (
+    start_time INTEGER,
+    theatre TEXT,
+    movie TEXT,
+    FOREIGN KEY (theatre) REFERENCES THEATRE(name),
+    FOREIGN KEY (movie) REFERENCES MOVIES(imdb_key),
+    PRIMARY KEY (start_time)
+)
+
+
+CREATE TABLE MOVIES (
+    title TEXT,
+    p_year TEXT,
+    imdb_key TEXT,
+    runtime TEXT
+    PRIMARY KEY (imdb_key)
+)
+
+
+CREATE TABLE TICKETS (
+    uuid TEXT DEFAULT (LOWER(HEX(RANDOMBLOB(16)))),
+    screening INTEGER,
+    FOREIGN KEY (screening) REFERENCES SCREENINGS(start_time),
+    PRIMARY KEY (uuid)
+)
+
+CREATE TABLE CUSTOMERS (
+    username TEXT,
+    full_name TEXT,
+    password ,
+    PRIMARY KEY (username)
+)
